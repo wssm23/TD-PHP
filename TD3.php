aab5443836ae9589c7f938adce5872410020ff96
wssm23 <wassimbensefia01@gmail.com>
2025-11-19 15:11:06 +0100

    TD3.php

--- TD3.php
+++ TD3.php
@@ -1,24 +1,21 @@
 <?php
 
-//  nouveaux contacts
+
 $nouveauxContacts = ["Mathieu LOUVEL", "John Doe", "Arya Stark", "Alice Dupont", "Jean Martin", "test"];
 
-// lire les contacts existants
 $contactsExistants = file("contact.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-// ouvrir le fichier en mode ajout
 $fichier = fopen("contact.txt", "a");
 
-// ajouter les contacts seulement s'ils n'existent pas
 foreach ($nouveauxContacts as $contact) {
     if (!in_array($contact, $contactsExistants)) {
         fwrite($fichier, $contact . PHP_EOL);
     }
 }
 
-// fermer le fichier
 fclose($fichier);
 
 echo "Contacts ajoutés avec succès !";
 
 ?>
+
